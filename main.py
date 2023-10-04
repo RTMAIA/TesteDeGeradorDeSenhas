9c18e5b828389ce9ba9df602c60a95df2e355eb0
Rafael Maia <rafaelteixeiramaiaa@gmail.com>
2023-10-04 16:55:37 -0300

    Segunda versao

--- main.py
+++ main.py
@@ -1,59 +1,61 @@
 import random
 
-abcdMin = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p' 'q','r','s','t','u','v','w','x','y','z']
-ABCDmaiusc =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-numeros = ['1','2','3','4','5','6','7','8','9','0']
-simbol = ['!','@','#','$','%','^','*','(',')','_','+','=','/','*','-','+',',','.','/',';','[',']']
+alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            '1','2','3','4','5','6','7','8','9','0',
+            '!','@','#','$','%','^','*','(',')','_','+','=','/','*','-','+',',','.','/',';','[',']']
+alfabetoTemp = list()
 senha = list()
-while True:
-    minCharacter = int(input('Quantos caracteres você deseja na sua senha?(Min 8 caracteres e maxim 14 caracteres): '))
-    if minCharacter <= 7 or minCharacter > 14:
-        print('\33[31mMínimo 8 caracteres! Maximo 14 caracteres\33[m')
-        continue
-    else:
-        break
 
-print('\nCaso todas as opcoes sejam 1 caracter, sera preenchido com minusculas.\n')
+print('\33[32m------Bem vindo ao programa de geracao de senhas aleatorias!-------\33[m')
 
 while True:
-    qtdLetraMaiuscula = int(input('Quantas letras maiusculas ?(minimo 1 maiuscula, maximo 4): '))
-    if qtdLetraMaiuscula <= 0 or qtdLetraMaiuscula  > 4 :
-        print('\33[31mMínimo 1 caractere! Maximo 4 caracteres\33[m')
-        continue
-    else :
-        for letra in range(0, qtdLetraMaiuscula):
-            senha.append(random.choice(ABCDmaiusc))
-        break
+    try:
+        qtdCaracteres = int(input('\nQuandos caracteres deseja?: '))
 
-while True :
-    qtdLetraMinuscula = int(input('\nQuantas letras minusculas (minimo 1 minuscula, maximo 4)?: '))
-    if qtdLetraMinuscula <= 0 or qtdLetraMinuscula > 4 :
-        print('\33[31mMínimo 1 caractere! Maximo 4 caracteres\33[m')
-    else:
-        for letra in range (0, qtdLetraMinuscula):
-            senha.append(random.choice(abcdMin))
-        break
+        if qtdCaracteres <= 7 :
+            print('\33[31mQuantidade minima de caracteres é 8!\33[m')
+            continue
+        elif qtdCaracteres > 14:
+            print('\33[31mQuantidade maxima de caracteres é 14!\33[m')
+            continue
+        else:
+            break
+    except:
+        print('\33[31mApenas numeros, por favor!\33[m')
 
-while True:
-    qtdNumeros = int(input('\nQuantos numeros ? (minimo 1 numero, maximo 4): '))
-    if qtdNumeros <= 0 or qtdNumeros > 4:
-        print('\33[31mMínimo 1 caractere! Maximo 4 caracteres\33[m')
-        continue
-    else :
-        for letra in range(0, qtdNumeros):
-            senha.append(random.choice(numeros))
-        break
+letrasMaiusculas = str(input('Deseja letras maiusculas ?: [S/N]').upper().strip())
+letrasMinusculas = str(input('Deseja letras minusculas ?: [S/N]').upper().strip())
+numeros = str(input('Deseja numeros ?: [S/N]').upper().strip())
+simbolos = str(input('Deseja simbolos ?: [S/N]').upper().strip())
 
-while True :
-    qtdSimbolos = int(input('\nQuantos simbolos? (minimo 1 simbolo, maximo 2): '))
-    if qtdSimbolos <= 0 or qtdSimbolos > 4 :
-        print('\33[31mMínimo 1 caractere! Maximo 4 caracteres\33[m')
-        continue
-    else :
-        for letra in range(0, qtdSimbolos):
-            senha.append(random.choice(simbol))
-        break
+if letrasMinusculas == 'S' and letrasMaiusculas == 'S' and numeros == 'S' and simbolos == 'S':
+    for l in range(0, qtdCaracteres):
+        senha.append(random.choice(alfabeto))
 
-random.shuffle(senha)
-for l in senha:
-    print(f'{l}',end='')
+elif letrasMinusculas == 'N':
+    for l in alfabeto:
+        if l == l.upper():
+            alfabetoTemp.append(l)
+    for l in range(0, qtdCaracteres):
+        senha.append(random.choice(alfabetoTemp))
+
+elif letrasMaiusculas == 'N':
+    for l in alfabeto:
+        if l == l.lower():
+            alfabetoTemp.append(l)
+    for l in range(0, qtdCaracteres):
+        senha.append(random.choice(alfabetoTemp))
+
+elif numeros == 'N':
+    alfabetoTemp = ''.join(l for l in alfabeto if not l.isdigit())
+    for l in range(0, qtdCaracteres):
+        senha.append(random.choice(alfabetoTemp))
+
+elif simbolos == 'N':
+    alfabetoTemp = ''.join(l for l in alfabeto if l.isalnum())
+    for l in range(0, qtdCaracteres):
+        senha.append(random.choice(alfabetoTemp))
+print('Sua nova senha é: ',end='')
+for item in senha:
+    print(f'{item}',end='')
